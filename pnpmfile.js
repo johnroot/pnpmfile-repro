9df8d46988a938f3c8f34be01fab40771769a787
Johnathon Root <johnathon@rev.com>
2019-06-04 14:16:21 -0700

    no respect at all

--- pnpmfile.js
+++ pnpmfile.js
@@ -6,6 +6,9 @@ module.exports = {
 
 function readPackage (pkg, context) {
     if (pkg.name === 'example') {
+        Object.assign(pkg.dependencies, {
+            'string-format': '*'
+        });
         Object.assign(pkg.peerDependencies, {
             'npm-package-example': '*'
         });
